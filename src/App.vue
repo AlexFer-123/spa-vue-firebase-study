1db57b67ce0f23e160306604e880b4a97f5c9cb4
alexandrefer1231 <alexandrefer1231@gmail.com>
2022-09-14 23:27:17 -0300

    start

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    Oi bb
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+
+  text-align: center;
+  color: #2c3e50;
+
+}
+</style>
